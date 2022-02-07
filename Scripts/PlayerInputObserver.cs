a688c15db5a03218984242fe56210de07b04779b
Hangarter <madison.paz@gmail.com>
2022-02-07 19:03:14 +0100

    Added all prefabs and scripts

---
+++ Scripts/PlayerInputObserver.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputObserver : MonoBehaviour
+{
+    public PlayerInputEvent OnLookEvent;
+
+    private void OnLook(InputValue value)
+    {
+        OnLookEvent.TriggerInputEvent(value);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        Debug.Log("On Move");
+        //onMove.TriggerInputEvent(inputAction);         
+    }
+
+}
